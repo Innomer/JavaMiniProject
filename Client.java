8c1d097026996a9e3bd62ef943af39693a3f93fe
anandakbari <anandakbari9@gmail.com>
2022-12-08 18:06:06 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,202 @@
+package project;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.*;
+import java.awt.Font;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+  
+public class Client extends JFrame 
+{
+    final static int ServerPort = 1234;
+    static String name;
+    static Vector <String>listofnames=new Vector<>();
+    
+
+    //delcaring gui components   
+    
+    private JTextArea messagearea=new JTextArea();
+    private JTextField messageinput=new JTextField();
+    JButton sendbutton=new JButton("Send");
+    private Font font=new Font("Roboto",Font.PLAIN,20);
+    private JLabel heading=new JLabel(name);
+
+    public Client() throws IOException{
+        try (Scanner scn = new Scanner(System.in)) {
+            System.out.print("Enter your name: ");
+            name = scn.nextLine();
+        }
+        listofnames.add(name);
+        // getting localhost ip
+        InetAddress ip = InetAddress.getByName("localhost");
+         
+        // establish the connection
+        Socket s = new Socket(ip, ServerPort);
+        
+        
+        
+        
+        // obtaining input and out streams
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        creategui(heading);
+        //handleEvents(s,dis,dos,name);
+        //startReading(s,dis,dos,name);
+        //startwriting(s,dis,dos,name);
+        dos.writeUTF(name);
+        // sendMessage thread
+         Thread sendMessage = new Thread(new Runnable() 
+        {
+            @Override
+            public void run() {
+
+               messageinput.addKeyListener(new KeyListener(){
+           
+                @Override
+                public void keyTyped(KeyEvent e) {
+                   
+                    
+                }
+    
+                @Override
+                public void keyPressed(KeyEvent e) {
+                   
+                    
+                }
+    
+                @Override
+                public void keyReleased(KeyEvent e) {
+                   
+                    //getkeycode gives code of key pressed 
+                    //10 is keycode for enter
+                    if(e.getKeyCode()==10){
+                        System.out.println("you pressed enter");
+                        //getting what u typed
+                        String contenttosend=messageinput.getText();
+                        if(contenttosend.length()>=1){
+                            
+                        //messagearea.append(name+": "+contenttosend+"\n");
+                        
+    
+                        try {
+                            dos.writeUTF(contenttosend);
+                            
+                        } catch (IOException e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                        }
+                        }
+                        //clearing the window for new text
+                        messageinput.setText("");
+                        
+    
+                    }
+                }
+    
+            });
+
+            sendbutton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                   
+                    String content=messageinput.getText();
+                    messagearea.append(name+": "+content+"\n");
+                    try {
+                        dos.writeUTF(content);
+                    } catch (IOException e1) {
+                        
+                        e1.printStackTrace();
+                    }
+                    messageinput.setText("");
+                    }
+                    //clearing the window for new text
+                    
+                
+            });
+            }
+        });
+          
+        // readMessage thread
+        Thread readMessage = new Thread(new Runnable() 
+        {
+            @Override
+            public void run() {
+                try {
+                while (true) {
+                    
+                        // read the message sent to this client
+                        String msg = dis.readUTF();
+                        if(msg.equals("logout")){
+                        //this.isloggedin=false;
+                        s.close();
+                        
+                        break;
+                }
+                        //System.out.println(msg);
+                        messagearea.append(msg+"\n");
+                    } 
+                }catch (IOException e) {
+  
+                        System.out.println("connection closed");
+                        
+                        
+                        
+                    }
+                }          
+        });
+  
+        sendMessage.start();
+        readMessage.start();
+  
+    }
+
+
+    private void creategui(JLabel heading){
+
+        this.setTitle("client messager[end]");
+        this.setSize(600,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //coding component
+        heading.setFont(font);
+        messagearea.setFont(font);
+        messageinput.setFont(font);
+        
+
+        //heading.setIcon(new ImageIcon("icon.png"));
+
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+
+        messagearea.setEditable(false);
+
+
+
+        //  setting frame's layout
+        this.setLayout(new BorderLayout());
+        this.add(heading,BorderLayout.NORTH);
+        JScrollPane jscrollpane=new JScrollPane(messagearea);
+        this.add(jscrollpane,BorderLayout.CENTER);
+        this.add(messageinput,BorderLayout.SOUTH);
+
+       sendbutton.setHorizontalAlignment(SwingConstants.CENTER);
+       sendbutton.setBounds(500,0,100,32);
+       messageinput.add(sendbutton);
+    
+        
+        this.setVisible(true);
+    }
+
+
+        
+    public static void main(String args[]) throws UnknownHostException, IOException 
+    {
+       
+        Client cp=new Client();
+        
+        
+    }
+}
---
+++ Server.java
@@ -0,0 +1,226 @@
+package project;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.*;
+import java.awt.Font;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+// Server class
+public class Server extends JFrame
+{
+
+       // Vector to store active clients
+       static Vector<ClientHandler> ar = new Vector<>();
+       static String name;
+     // counter for clients
+       
+       static Vector <Integer>portnumber=new Vector<>();
+       static Vector <String>listofnames=new Vector<>();
+
+        //delcaring gui components
+    private JLabel heading=new JLabel("Server");
+    public static JTextArea messagearea=new JTextArea();
+    //private JTextField messageinput=new JTextField();
+    JButton sendbutton=new JButton("Send");
+    private Font font=new Font("Roboto",Font.PLAIN,20);
+    
+    
+        Socket s;
+
+     public Server() throws IOException{
+        
+        // server is listening on port 1234
+        
+        ServerSocket ss = new ServerSocket(1234);
+
+        Socket s;
+       
+        
+         creategui();
+     
+     
+     // running infinite loop for getting
+     // client request
+     while (true&&Server.ar.size()<=2)
+     {
+         // Accept the incoming request
+         
+         s = ss.accept();
+         
+         
+         int port=s.getPort();
+         portnumber.add(port);
+
+         System.out.println("New client request received : " + s);
+          
+         // obtain input and output streams
+         DataInputStream dis = new DataInputStream(s.getInputStream());
+         DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+         name=dis.readUTF();
+         System.out.println("Creating a new handler for this client... "+name);
+         listofnames.add(name);
+         // Create a new handler object for handling this request.
+         ClientHandler mtch = new ClientHandler(s,Server.name, dis, dos);
+
+         // Create a new Thread with this object.
+         Thread t = new Thread(mtch);
+          
+         System.out.println("Adding this client to active client list");
+         for(ClientHandler mc:ar)
+         mc.dos.writeUTF(name+" is ready to chat ");
+         messagearea.append(name+" is ready to chat \n");
+         // add this client to active clients list
+         Server.ar.add(mtch);
+         
+         // start the thread.
+         t.start();
+
+         // increment i for new client.
+         // i is used for naming only, and can be replaced
+         // by any naming scheme
+         //i++;
+     }
+    }
+
+    private void creategui(){
+    
+        this.setTitle("Server[end]");
+        this.setSize(600,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //coding component
+        heading.setFont(font);
+        messagearea.setFont(font);
+        
+        
+    
+        //heading.setIcon(new ImageIcon("icon.png"));
+    
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+    
+        messagearea.setEditable(false);
+        
+    
+    
+    
+        //  setting frame's layout
+        this.setLayout(new BorderLayout());
+        this.add(heading,BorderLayout.NORTH);
+        JScrollPane jscrollpane=new JScrollPane(messagearea);
+        this.add(jscrollpane,BorderLayout.CENTER);
+    
+        
+        this.setVisible(true);
+    }
+        
+   
+    public static void main(String[] args) throws IOException
+    {
+    
+        Server sc=new Server();
+        //Creatinggui cig=new Creatinggui();
+}
+}
+
+ 
+// ClientHandler class
+class ClientHandler implements Runnable 
+{
+    Scanner scn = new Scanner(System.in);
+    private String name;
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    Socket s;
+    boolean isloggedin;
+    
+       
+    
+    // constructor
+    public ClientHandler(Socket s, String name,
+                            DataInputStream dis, DataOutputStream dos) {
+        this.dis = dis;
+        this.dos = dos;
+        this.name = name;
+        this.s = s;
+        this.isloggedin=true;
+        
+    }
+    
+
+    
+    @Override
+    public void run() {
+ 
+        String received;
+        try
+            {  
+        while (true)
+        {
+             
+                int port=s.getPort();
+                // receive the string
+                received = dis.readUTF();
+                
+                for(int i=0;i<Server.listofnames.size();i++){
+                if(Server.portnumber.get(i)==s.getPort()){
+                System.out.println(Server.listofnames.get(i)+": "+received);
+                Server.messagearea.append(Server.listofnames.get(i)+": "+received+"\n");
+                }
+                
+                }
+                if(received.equals("logout")){
+                    this.isloggedin=false;
+                    this.s.close();
+                    break;
+                }
+                 
+                for (ClientHandler mc : Server.ar)
+                {
+                    if(received.equals("logout")){
+                        this.isloggedin=false;
+                        this.s.close();
+                        
+                        break;
+                    }
+                    // if the recipient is found, write on its
+                    // output stream
+                    if (mc.isloggedin==true)
+                    {
+                        if(Server.listofnames.size()==1){
+                            mc.dos.writeUTF(Server.listofnames.get(0)+" : "+received);
+                        }
+                        else{
+                            for(int i=0;i<Server.listofnames.size();i++){
+                        if(Server.portnumber.get(i)==port)  mc.dos.writeUTF(Server.listofnames.get(i)+" : "+received);
+                            }
+    
+                        }
+                         
+                    }
+                }
+            }
+         } catch (Exception e) {
+                 
+                e.printStackTrace();
+            }
+             
+        
+        try
+        {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+             
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
